45c275ec7b8d77e540ab022f2781d3cb70a63e2b
Zakk <zakk@rsdio.com>
2025-11-24 19:43:40 -0500

    Remove attempt at setting up firefox sync. It didn't really work anyways.

--- ansible/roles/firefox/files/sync-data/prefs.js
+++
@@ -1,7 +0,0 @@
-user_pref("services.sync.username", "zakk@rsdio.com");
-user_pref("services.sync.engine.addons", true);
-user_pref("services.sync.engine.bookmarks", true);
-user_pref("services.sync.engine.history", true);
-user_pref("services.sync.engine.passwords", true);
-user_pref("services.sync.engine.prefs", true);
-user_pref("services.sync.engine.tabs", true);
