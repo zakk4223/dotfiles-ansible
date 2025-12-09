2142e752e855b5edeb68afcb9f11a074b08d3f82
Zakk <zakk@rsdio.com>
2025-12-09 12:55:17 -0500

    Add fzf to zsh, and add generic fzf_search() function

---
+++ ansible/roles/zsh/files/hosts/common/.config/zsh/00_fzf.zsh
@@ -0,0 +1,9 @@
+fzf_select() {
+	fzf --sync --query $1 --bind 'result:transform:
+	  if [[ -n $FZF_QUERY ]]; then
+	    echo "track-current+clear-query"
+	  else
+	    echo "untrack-current+offset-middle+unbind(result)"
+	  fi
+	'
+}
