64ad26520a432be4a7312c293892093e35f55d43
Zakk <zakk@rsdio.com>
2026-02-08 06:14:29 -0500

    add awww

---
+++ ansible/roles/awww/files/hosts/common/bin/awww_random.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+if [[ $# -lt 1 ]] || [[ ! -d $1   ]]; then
+	echo "Usage:
+	$0 <dir containg images>"
+	exit 1
+fi
+
+WALLPAPER=$(find $1 -type f -print0 | shuf -zn1 | xargs -0)
+
+
+ awww img $WALLPAPER
+ rm ~/Pictures/current_wallpaper
+ ln -s $WALLPAPER ~/Pictures/current_wallpaper
