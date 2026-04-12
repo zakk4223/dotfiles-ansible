18288eedb43327c8fabbca964a60e1c1ec71dd1a
Zakk <zakk@rsdio.com>
2026-04-11 23:59:29 -0400

    Change tmx function to just invoke a sesh chooser

--- ansible/roles/tmux/files/hosts/common/.config/zsh/tmux.sh
+++ ansible/roles/tmux/files/hosts/common/.config/zsh/tmux.sh
@@ -1,8 +1,3 @@
 tmx() {
-    local session_name=${1:-${PWD:t}}
-	  if [ -z "$TMUX" ]; then
-		  command tmux new-session -A -s "$session_name"
-	  else
-		  command tmux new-session -Ad -s "$session_name"; command tmux switch-client -t "$session_name"
-    fi
+		sesh connect $(sesh list | fzf)
 }
